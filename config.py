87f774cea92beadcfcb9484852a5b09c81cab6fd
caincoan12 <67595382+caincoan12@users.noreply.github.com>
2021-02-10 10:35:54 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+{
+  "username": "...",
+  "channel": "...",
+  "oauth_token": "...",
+  "client_id": "...",
+  "youtube_api_key": "...",
+  "youtube_playlists": {
+    "explains": {
+      "playlist_id": "PLWBKAf81pmOaP9naRiNAqug6EBnkPakvY",
+      "github": "https://github.com/anthonywritescode/explains"
+    },
+    "faq": {
+      "playlist_id": "PLWBKAf81pmOZEPeIV2_pIESK5hRMAo1hR"
+    }
+  },
+  "airnow_api_key": "..."
+}
