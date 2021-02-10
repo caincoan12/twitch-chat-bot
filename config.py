cc0e3f268c137735ba8a21daedb54d5d94d03713
caincoan12 <67595382+caincoan12@users.noreply.github.com>
2021-02-10 10:38:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 {
-  "username": "...",
-  "channel": "...",
+  "username": "Noctis:)",
+  "channel": "hzn_ghostyt",
   "oauth_token": "...",
-  "client_id": "...",
+  "client_id": "7nlwd404ncoez59zvwjpupxqfz7ois",
   "youtube_api_key": "...",
   "youtube_playlists": {
     "explains": {
       "playlist_id": "PLWBKAf81pmOaP9naRiNAqug6EBnkPakvY",
-      "github": "https://github.com/anthonywritescode/explains"
+      "github": "https://github.com/caincoan12/twitch-chat-bot"
     },
     "faq": {
       "playlist_id": "PLWBKAf81pmOZEPeIV2_pIESK5hRMAo1hR"
